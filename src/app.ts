06c632c611f0ebc517c10a67f589e5a7e0b7960f
GabrielGQA <gabrielgqa@gmail.com>
2025-07-30 14:07:13 -0300

    feat: autoriza cors para app frontend

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import routes from './routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(routes);
 
